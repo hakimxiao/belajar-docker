fc77eb86ca5ade4382208a14ece026a8185ef491
hakimxiao <alhakimabusolihin@gmail.com>
2026-06-27 00:08:14 +0700

    docker CONTAINR STATS

---
+++ docker-dasar/docker-container-stats.sh
@@ -0,0 +1,4 @@
+# Container stats
+    # digunakan untuk melihat statistik container seperti memory, cpu, network, dan lain-lain 
+    # (tidak hanya container yang sedang berjalan, namun juga container yang sudah mati) jadi ini digunakan untuk melihat statistik
+docker container stats
